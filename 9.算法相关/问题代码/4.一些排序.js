a822f59bfd4d3e177ba531376318e9622333d7f2
hbw-cloud <2316570512@qq.com>
2019-02-15 00:03:18 +0800

    完善

--- 9.算法相关/问题代码/4.一些排序.js
+++ 9.算法相关/问题代码/4.一些排序.js
@@ -2,12 +2,18 @@
 //简述一下他的思想：对数组元素每一项与数组里面的剩余项进行比较，如果大就交换，这样大的就被冒泡到右边。
 
 let boboSort = (array) => {
-    for(let i = 0; i< array; i++) {
-        for(let j=1; j< array-1 -i; j++) {
-            if(array[i] > array[j]) {
-                [array[i], array[j]] = [array[j], array[i]]
+    let length = array.length
+    for(let i = 0; i< length; i++) {
+        for(let j=0; j< length -i; j++) {
+            if(array[j] > array[j+1]) {
+                // [array[i], array[j]] = [array[j], array[i]]
+                let temp =array[j+1];
+                array[j+1] = array[j];
+                array[j] =temp;
             }
         }
     }
 }
-let testAarr
+let testAarr = [1,5,7,1,56,1,3,5,4,6]
+boboSort(testAarr);
+console.log(testAarr);
