7fbafe139594616629d7aa69b05f6e4d93178a56
hbw-cloud <2316570512@qq.com>
2019-02-19 10:57:19 +0800

    转换字符

--- 3.JS/代码库/reg.js
+++ 3.JS/代码库/reg.js
@@ -190,3 +190,13 @@
 // ["-01", "01", 4, "2017-01-06"]
 // ["-06", "06", 7, "2017-01-06"]
 // "2017undefinedundefined"
+
+let str = "I like beijing.";
+function order(str) {
+    let res = str.split(' ').map(item => item.split('').reverse().join(''));
+
+    let end =   res.reverse().map(item => item.split('').reverse().join(''));
+    return end.join(' ')
+}
+
+console.log(end.join(' '))
